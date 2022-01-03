cc0cf4a9b74b73a82c70360a8a606fced83b6344
yagu238 <yagu238.dev@gmail.com>
2022-01-03 22:34:13 +0900

    todo完了を表示するように修正。

--- apps/web/src/pages/todo/todo-list-item.page.tsx
+++ apps/web/src/pages/todo/todo-list-item.page.tsx
@@ -1,16 +1,26 @@
 import { memo } from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Text } from '@chakra-ui/react';
 import { Todo } from '@todo/api-interfaces';
+import { CheckCircleIcon } from '@chakra-ui/icons';
 
 interface Props {
   todo: Todo;
 }
 
 export const TodoListItem = memo((props: Props) => {
+  const stateColor = props.todo.completed ? 'teal.500' : 'gray.100';
+
   return (
     <Box p={5} shadow="md" borderWidth="1px">
-      <Heading fontSize="xl">{props.todo.title}</Heading>
-      <Text mt={4}>test................</Text>
+      <Flex>
+        <Center w="60px">
+          <CheckCircleIcon w={8} h={8} color={stateColor}></CheckCircleIcon>
+        </Center>
+        <Box flex="1" ml={4}>
+          <Heading fontSize="xl">{props.todo.title}</Heading>
+          <Text mt={4}>test................</Text>
+        </Box>
+      </Flex>
     </Box>
   );
 });
